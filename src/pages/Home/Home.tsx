6e229e352a5e9a1aba2d4bebffc5b9d539b9fa72
maardgs <mariliarodrigues.94@gmail.com>
2024-08-02 21:57:12 -0300

    :tada: First Commit

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,16 @@
+import "./Home.css"
+
+function Home(){
+    return(
+        <>
+        <h1 className="titulo">H O M E</h1>
+        <p className="dot">----------------------------------</p>
+        <img className="photo" src="https://images.unsplash.com/photo-1716975152909-394356420025?q=80&w=1065&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="street" />
+       
+        </>
+       
+    )
+}
+
+
+export default Home;
