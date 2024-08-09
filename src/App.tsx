5be33e7d15fb1054b2a3a5acabfd0f78cdbad010
maardgs <mariliarodrigues.94@gmail.com>
2024-08-09 14:46:11 -0300

    :construction:Implementa sistema de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import './App.css';
-import Home from './pages/home/Home';
-import Navbar from './componentes/navbar/Navbar';
-import Footer from './componentes/footer/Footer';
+import React from "react";
+import "./App.css";
+import Home from "./pages/home/Home";
+import Navbar from "./componentes/navbar/Navbar";
+import Footer from "./componentes/footer/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
     <>
-  <Navbar/>
-  <Home/>
-  <Footer/>
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </>
-);
+  );
 }
 
 export default App;
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Login() {
+    let navigate = useNavigate()
     return (
         <>
-            <div className="h-96 mb-96">
-
-                <h1 className='text-xl'>Login</h1>
-
+            <div>
+            <h2 className="text-slate-900 text-5xl   m-4">Login</h2>
+            <div>
+                <button type='submit'
+                    onClick={() => { navigate('/home') }}
+                    className='hover:underline mx-4'>
+                    Login useNavigate
+                </button>
+                <Link to='/home' className='hover:underline  mx-4'>Login por Link</Link>
             </div>
+
+        </div>
         </>
     );
 }
