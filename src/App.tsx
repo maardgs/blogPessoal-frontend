7301d4fb9e8e72237979bef34a75a71625ec3811
maardgs <mariliarodrigues.94@gmail.com>
2024-08-09 14:06:12 -0300

    :sparkles:Adiciona layout da Home com Navbar e Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/home/Home';
-import Login from './pages/login/Login';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './componentes/navbar/Navbar';
+import Footer from './componentes/footer/Footer';
 
 function App() {
   return (
     <>
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/home' element={<Home/>} />
-    </Routes>
-  </BrowserRouter>
+  <Navbar/>
+  <Home/>
+  <Footer/>
     </>
 );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,22 +1,30 @@
 import React from "react";
 import "./Home.css";
-import { Link, useNavigate } from "react-router-dom";
+import { House } from "@phosphor-icons/react";
+
 
 function Home() {
-  let navigate = useNavigate();
   return (
-    <div>
-      <h1 className="text-slate-900 text-5xl m-4">Home</h1>
-      <div>
-        <button type="submit"
-            className="hover:underline mx-4"
-            onClick={() => {navigate("/login") }} >
-          login useNavigate
-        </button>
-        <Link to='/login' className='hover:underline mx-4'> Login por link </Link>
+    <>
+      <div className="bg-indigo-900 flex justify-center">
+        <div className="container grid grid-cols-2 text-white">
+          <div className="flex flex-col gap-4 items-center justify-center py-4">
+            <h2 className="text-5xl font-bold">Seja bem vinde!</h2>
+            <p className="text-xl">Expresse aqui seus pensamentos e opniões</p>
+            
+            <div className="flex justify-around gap-4">
+
+            <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
+              </div>
+            </div>
+  
+            <div className="flex justify-center ">
+              <img src={House} alt="" className='w-2/3' />
+
+          </div>
+        </div>
       </div>
-      
-    </div>
+    </>
   );
 }
 
