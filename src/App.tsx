88f4db8a5712685a02caf3a4e082090128168255
maardgs <mariliarodrigues.94@gmail.com>
2024-08-12 09:51:51 -0300

    :wrench:Implementa integração com API usando Axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from "react";
-import "./App.css";
-import Home from "./pages/home/Home";
-import Navbar from "./componentes/navbar/Navbar";
-import Footer from "./componentes/footer/Footer";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./pages/login/Login";
+import React from 'react';
+import UserProvider from './contexts/UserContext';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+
 
 function App() {
+
   return (
-    <>
+    <UserProvider>
       <BrowserRouter>
-      <Navbar/>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
         </Routes>
-        <Footer/>
+
       </BrowserRouter>
-    </>
+
+    </UserProvider>
   );
 }
 
