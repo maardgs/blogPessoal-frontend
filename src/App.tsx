9d6fed674a0bdf56b67af207a459fb6854521e80
maardgs <mariliarodrigues.94@gmail.com>
2024-08-12 10:27:27 -0300

    :bricks:Construindo o template do Componente Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import './App.css';
 
-import Footer from './componentes/footer/Footer';
-import Navbar from './componentes/navbar/Navbar';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/login/Login';
 import Cadastro from './pages/cadastro/Cadastro';
 import Home from './pages/home/Home';
+import { AuthProvider } from './contexts/AuthContext';
 
 
 function App() {
   return (
     <>
-   
+    <AuthProvider>
         <BrowserRouter>
           <Navbar />
           <div className='min-h-[80vh]'>
             <Routes>
-              <Route path="/" element={<Cadastro />} />
+              <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
@@ -25,7 +26,7 @@ function App() {
           </div>
           <Footer />
         </BrowserRouter>
-  
+        </AuthProvider>
     </>
   );
 }
