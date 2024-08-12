55ae64838687440217270d0979f56732a338a38d
maardgs <mariliarodrigues.94@gmail.com>
2024-08-12 15:35:30 -0300

    :bug:Correções de bug

--- src/componentes/navbar/Navbar.tsx
+++ src/componentes/navbar/Navbar.tsx
@@ -24,10 +24,10 @@ function Navbar() {
           <div className="container flex justify-between text-lg">
           <Link to='/home' className='text-2xl font-bold uppercase'>Blog Pessoal</Link>
 
-            <div className='flex gap-4'>
+          <div className='flex gap-4'>
             <div className='hover:underline'>Postagens</div>
             <Link to='/temas' className='hover:underline'>Temas</Link>
-              <div className='hover:underline'>Cadastrar tema</div>
+            <Link to='/cadastroTema' className='hover:underline'>Cadastrar tema</Link>
               <div className='hover:underline'>Perfil</div>
               <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
              
