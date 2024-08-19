cc26fe130d71105d7dfca35c593961bbfd216fde
maardgs <mariliarodrigues.94@gmail.com>
2024-08-18 22:48:12 -0300

    :rocket:Deploy

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://blogpessoal-cren.onrender.com'
+  baseURL: import.meta.env.VITE_API_URL
 })
-// https://blogpessoal-cren.onrender.com https://db-blogpesoal-clvr.onrender.com/
+
 export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) => {
   const resposta = await api.post(url, dados)
   setDados(resposta.data)
